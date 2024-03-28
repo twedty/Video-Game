534de748666d295d9e4d8f63139f456c5208f5e7
twedty <jamestwedt@gmail.com>
2024-03-27 21:02:09 -0400

    added click events to every single image on screen

---
+++ index.js
@@ -0,0 +1,65 @@
+const character = document.getElementById('character')
+character.addEventListener('click', function() {
+    const element = document.getElementById("main-box")
+    element.textContent = 'This is YOU. By you clicking you, you have now made this become way more complicated than you should have. You should be disappointed in yourself.'
+})
+
+const chair = document.getElementById('chair')
+chair.addEventListener('click', function() {
+    const element = document.getElementById("main-box")
+    element.textContent = 'A place that you can sit on.'
+})
+
+const coffeeTable = document.getElementById('coffee-table')
+coffeeTable.addEventListener('click', function() {
+    const element = document.getElementById("main-box")
+    element.textContent = 'This is something you can set drinks, food, and other items to hold for you at an elevated space.'
+})
+
+const controller = document.getElementById('controller')
+controller.addEventListener('click', function() {
+    const element = document.getElementById("main-box")
+    element.textContent = 'A tongue licks a lollipop and a broken tooth requires a fix. John Wicks dog was killed so he went on a rampage for kicks. One word with three letters rhymes with this theme so you better figure it out quick!'
+})
+
+const couch = document.getElementById('couch')
+couch.addEventListener('click', function() {
+    const element = document.getElementById("main-box")
+    element.textContent = 'I am what makes a potato......a COUCH potato.'
+})
+
+const lamp = document.getElementById('lamp')
+lamp.addEventListener('click', function() {
+    const element = document.getElementById("main-box")
+    element.textContent = 'I multiply like rabbits in ways that are insane. But multiply me in one way and all the numbers are the same.'
+})
+
+const remote = document.getElementById('remote')
+remote.addEventListener('click', function() {
+    const element = document.getElementById("main-box")
+    element.textContent = 'As a word with four letters I can mean one way or another. "No" in one way and in the other, a number.'
+})
+
+const safe = document.getElementById('safe')
+safe.addEventListener('click', function() {
+    const element = document.getElementById("main-box")
+    element.textContent = 'Insert code combination here:'
+})
+
+const stand = document.getElementById('stand')
+stand.addEventListener('click', function() {
+    const element = document.getElementById("main-box")
+    element.textContent = 'I hold something very valuable....'
+})
+
+const table = document.getElementById('table')
+table.addEventListener('click', function() {
+    const element = document.getElementById("main-box")
+    element.textContent = 'What has legs but cannot walk? A table.'
+})
+
+const tv = document.getElementById('tv')
+tv.addEventListener('click', function() {
+    const element = document.getElementById("main-box")
+    element.textContent = 'The fox, the dog, and the cat ate one red bat. What is the one thing you can find in common with what you know of that fact?'
+})
